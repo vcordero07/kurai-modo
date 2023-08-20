4f0ab1f7a4fb7ea45d04366be30f34fc3b8f0659
vcordero07 <vcordero07@gmail.com>
2023-08-19 23:03:07 -0400

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
-import { ThemeContext } from './ThemeContext';
+import DarkLightToggle from './dark-light-toggle';
 
-import DarkToggle from './DarkToggle';
-
-const Header = ({ siteTitle }) => {
-  return (
+const Header = ({ siteTitle }) => (
+  <OuterWrapper>
     <Wrapper>
-      {siteTitle}
-      <DarkToggle />
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+      <DarkLightToggle />
     </Wrapper>
-  );
+  </OuterWrapper>
+);
+
+const OuterWrapper = styled.header`
+  background: rgba(128, 128, 128, 0.1);
+  margin-bottom: 1.45rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 };
 
-const Wrapper = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 16px;
-`;
+Header.defaultProps = {
+  siteTitle: ``,
+};
 
 export default Header;
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Layout from './Layout'
-import Seo from './Seo'
-import Title from './Title'
-import Footer from './Footer'
-
-export { Layout, Seo, Title, Footer }
